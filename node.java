055504e66f2625f3d1d5707096c49721333d9d7c
evanalbers <62731190+evanalbers@users.noreply.github.com>
2022-04-19 12:37:15 -0400

    starting

---
+++ node.java
@@ -0,0 +1,12 @@
+//AUTHOR: Evan Albers
+//DATE CREATED: April 19 2022
+//PURPOSE: Abstract class representing a user on the network
+
+public class node
+{
+
+    private String privateKey;
+    public String publicKey;
+
+
+}
