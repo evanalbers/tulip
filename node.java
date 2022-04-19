261032bcea2a1fd11cadda7415d5ccc7bf3fca61
evanalbers <62731190+evanalbers@users.noreply.github.com>
2022-04-19 12:43:37 -0400

    yeah

--- node.java
+++ node.java
@@ -4,9 +4,33 @@
 
 public class node
 {
-
+    //basic idea of a node: has a private key, public key, balance of tulips
     private String privateKey;
     public String publicKey;
+
+    private long balance = 0;
+
+
+    //constructor 
+    public node(String priKey, String pubKey)
+    {
+
+            privateKey = priKey; //setting keys
+            publicKey = pubKey;
+
+    }
+
+    //used to modify the balance of a node's tulips 
+    public modifyBalance(long change)
+    {
+
+        balance += change;
+
+    }
+
+
+
+
 
 
 }
